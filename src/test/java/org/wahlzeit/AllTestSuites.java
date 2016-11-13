13a7655c9d55bcf4275dce6bd178340058a75e52
juliagawellek <julia.gawellek@gmx.net>
2016-11-13 23:13:44 +0100

    revision of adap-cw04 homework

--- src/test/java/org/wahlzeit/AllTestSuites.java
+++ src/test/java/org/wahlzeit/AllTestSuites.java
@@ -4,19 +4,24 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.wahlzeit.handlers.HandlersTestSuite;
 import org.wahlzeit.model.ModelTestSuite;
-//import org.wahlzeit.model.persistence.PersistenceTestSuite;
 import org.wahlzeit.services.ServicesTestSuite;
-//import org.wahlzeit.testEnvironmentProvider.EnvironmentProviderTestSuite;
 import org.wahlzeit.utils.UtilsTestSuite;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+
+
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	HandlersTestSuite.class, ModelTestSuite.class, ServicesTestSuite.class, 
 	UtilsTestSuite.class,
-	//PersistenceTestSuite.class, 
-	//EnvironmentProviderTestSuite.class,
 })
 
 public class AllTestSuites {
 
+	private final LocalServiceTestHelper helper =
+			new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig()
+			    .setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
 }
