e44dcb8b44863420fca3a1ae7c2fbc787d18e03e
juliagawellek <julia.gawellek@gmx.net>
2017-01-16 11:24:02 +0100

    added toString, equals and hashCode methods

--- src/main/java/org/wahlzeit/model/Bridge.java
+++ src/main/java/org/wahlzeit/model/Bridge.java
@@ -18,6 +18,42 @@ public class Bridge {
 		}
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bridgeType == null) ? 0 : bridgeType.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bridge other = (Bridge) obj;
+		if (bridgeType == null) {
+			if (other.bridgeType != null)
+				return false;
+		} else if (!bridgeType.equals(other.bridgeType))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Bridge [bridgeType=" + bridgeType + ", owner=" + owner + "]";
+	}
+
 	public BridgeType getType() {
 		return bridgeType;
 	}
